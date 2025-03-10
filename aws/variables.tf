a053845e58c7acd36a5c1cf95b1205f3b0d46705
marcothedood <marco.medde@outlook.it>
2025-03-10 22:08:14 +0100

    replaced credential filepath with placeholder

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,24 +1,24 @@
 variable "shared_credentials_file" {
   description = "Path to the AWS credentials file"
   type = string
-  default = "/Users/mme/.aws/credentials"
+  default = "replace with your path"
 }
 
 variable "aws_region" {
   description = "AWS region"
   type = string
-  default = "eu-north-1"
+  default = "eu-north-1" # Stockholm
 }
 
 variable "aws_ami" {
   description = "AMI to use for the instance"
   type = string
-  default = "ami-0717dacde6ae13b14"
+  default = "ami-0717dacde6ae13b14" # Red Hat Enterprise Linux 8
 }
 
 variable "instance_type" {
   description = "Instance type"
   type = string
-  default = "t3.xlarge"
+  default = "t3.xlarge" # 4 vCPUs, 16 GB RAM 
 }
 
