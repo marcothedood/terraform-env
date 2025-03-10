3cc0a0b600d2db118ad9071cf5a6f2f69a14fac0
marcothedood <marco.medde@outlook.it>
2025-03-10 21:52:50 +0100

    Tackling VPC - Security Groups w/ rules - Elastic IP and Subnet

--- aws/main.tf
+++ aws/main.tf
@@ -20,3 +20,47 @@ resource "aws_instance" "devmachine" {
     Name = "devmachine"
   }
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group" "default" {
+  name = "terraform-security-group"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  security_group_id = aws_security_group.default.id
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+  security_group_id = aws_security_group.default.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["213.55.241.218/32"]
+}
+
+resource "aws_security_group_rule" "allow_https_inbound" {
+  security_group_id = aws_security_group.default.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_eip" "instance" {
+  instance = aws_instance.devmachine.id
+  vpc = true
+}
