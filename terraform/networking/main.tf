6994b2715eee6a2db82cc38ccd3af3c912553410
marcothedood <marco.medde@outlook.it>
2025-03-13 00:45:55 +0100

    Implementing matomo provisioning module and small tweaks to Terraform config

--- terraform/networking/main.tf
+++ terraform/networking/main.tf
@@ -16,11 +16,12 @@ resource "aws_security_group" "default" {
 resource "aws_security_group_rule" "allow_http_inbound" {
   security_group_id = aws_security_group.default.id
   type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
 }
+
 
 resource "aws_security_group_rule" "allow_ssh_inbound" {
   security_group_id = aws_security_group.default.id
@@ -68,7 +69,17 @@ data "aws_route53_zone" "primary" {
 
 resource "aws_route53_record" "primary" {
   zone_id = data.aws_route53_zone.primary.zone_id
-  name    = "devm.doodlab.xyz"
+  name    = "matomo.doodlab.xyz"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_eip.instance.public_ip]
+  allow_overwrite = true
+  depends_on = [aws_eip_association.eip_assoc]
+}
+
+resource "aws_route53_record" "secondary" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "www.matomo.doodlab.xyz"
   type    = "A"
   ttl     = "300"
   records = [aws_eip.instance.public_ip]
